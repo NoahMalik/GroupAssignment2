f54d28077897dedb4c82c0f3f80001be1525ea7e
NoahMalik <nhmkb9@umsystem.edu>
2025-03-05 15:53:09 -0600

    Fixing the character manager

--- src/edu/mu/earth/characters/EarthCharacter.java
+++ src/edu/mu/earth/characters/EarthCharacter.java
@@ -1,26 +1,44 @@
 package edu.mu.earth.characters;
 
-public class EarthCharacter {
-	protected String name;
-    protected double health;
-    protected double power;
+public abstract class EarthCharacter {
+	private String name;
+    private double health;
+    private double power;
 
-    // Constructor
-    public MiddleEarthCharacter(String name, double health, double power) {
+    public EarthCharacter(String name, double health, double power) {
         this.name = name;
         this.health = health;
         this.power = power;
     }
 
-    // Abstract methods
-    public abstract boolean attack(MiddleEarthCharacter target);
+    public abstract boolean attack(EarthCharacter target);
     public abstract String getRace();
 
-    // Concrete method to display character info
+    // Getter methods for name, health, and power
+    public String getName() {
+        return name;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    // Setter methods for health, and power
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
     public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Race: " + getRace());
-        System.out.println("Health: " + health);
-        System.out.println("Power: " + power);
+    	System.out.println("Name: " + getName());
+    	System.out.println("Race: " + getRace());
+    	System.out.println("Health: " + getHealth());
+    	System.out.println("Power: " + getPower());
     }
 }
